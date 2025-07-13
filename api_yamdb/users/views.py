bb962106773b5970d8a438b33264d1258dc54875
M Yaroslav <workcross@gmail.com>
2025-07-14 01:02:45 +0530

    #015 поправлены импорты лишних библиотекÐ

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import permissions, response, status, views, viewsets
-from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 
